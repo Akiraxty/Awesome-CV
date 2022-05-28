f35c039a8d24c1f47f43f33e2583cdab4abd3241
xutianyu <1743445463@qq.com>
2022-05-28 12:19:37 +0800

    master

--- configs/common/base_module.py
+++ configs/common/base_module.py
@@ -7,4 +7,5 @@ class BaseModule(nn.Module):
         super(BaseModule, self).__init__()
         self._is_init=False
         self.init_cfg=copy.deepcopy(init_cfg)
-        w1
+        w1qwr
+        
