425c8b89c32a587a19259cce715c039b5b0f5c3c
xutianyu <1743445463@qq.com>
2022-05-28 12:19:02 +0800

    master

---
+++ train.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def main(args):
+    
+    return
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config')
+    args = parser.parse_args()
+    main(args)
