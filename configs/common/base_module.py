8096652f5d2cdcfa7f547ce4b79e9be2a3d2ca1f
xutianyu <1743445463@qq.com>
2022-05-28 12:15:58 +0800

    1743445463@qq.com

---
+++ configs/common/base_module.py
@@ -0,0 +1,10 @@
+from copy import copy
+from mimetypes import init
+import torch.nn as nn
+import copy
+class BaseModule(nn.Module):
+    def __init__(self, init_cfg=None) -> None:
+        super(BaseModule, self).__init__()
+        self._is_init=False
+        self.init_cfg=copy.deepcopy(init_cfg)
+        
